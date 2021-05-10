8d020d6eba1f13820341def4f9859debe69b2a0f
Ravie403 <info@tkm.blue>
2021-05-11 01:10:04 +0900

    add heroku settings

---
+++ app/app/prod.py
@@ -0,0 +1,18 @@
+from .settings import *
+
+DEBUG=False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
+
+STATIC_ROOT = None
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
