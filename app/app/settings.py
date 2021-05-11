ba583d235ab51208cb0cb256d5460c38da668d18
Ravie403 <info@tkm.blue>
2021-05-11 20:53:48 +0900

    これでうまくいったらキレる

--- app/app/settings.py
+++ app/app/settings.py
@@ -14,8 +14,7 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-print(BASE_DIR)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
