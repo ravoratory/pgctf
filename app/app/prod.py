7557b834367d278ed1cc2ee9850a4fc97fc08c9c
Ravie403 <info@tkm.blue>
2021-05-11 20:05:19 +0900

    Update prod.py

--- app/app/prod.py
+++ app/app/prod.py
@@ -12,7 +12,5 @@ DATABASES = {
     }
 }
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+
