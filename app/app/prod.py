9eda3cbbdd5de825297920a9b0a3a4e707a95497
Takuma Honjo <info@tkm.blue>
2021-05-11 21:01:12 +0900

    Herokuと接続 (#2)
    * 全てを諦めた

---
+++ app/app/prod.py
@@ -0,0 +1,14 @@
+from .settings import *
+
+DEBUG=False
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASSWORD'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ['DB_PORT'],
+    }
+}
+
