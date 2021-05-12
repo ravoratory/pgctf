238412c04671f420d9ce14dd952963f222fc7fe7
disVill <konokou2000@gmail.com>
2021-05-12 19:40:45 +0900

    Create sign up in view

--- app/users/views.py
+++ app/users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.http import HttpResponseRedirect
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views import generic
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .models import User
+
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = "users/signup.html" 
+    success_url = reverse_lazy('users:signin')
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        self.object = user 
+        return HttpResponseRedirect(self.get_success_url())
+
+
+class SignInView(generic.View):
+    def post(self, request, *arg, **kwargs):
+        form = AuthenticationForm(data=request.POST)
+        if not form.is_valid():
+            return render(request, 'users/signin.html', {'form': form})
+        username = form.cleaned_data.get('username')
+        user = User.objects.get(username=username)
+        login(request, user)
+        return redirect('/')
+
+    def get(self, request, *args, **kwargs):
+        form = AuthenticationForm(request.POST)
+        return render(request, 'users/signin.html', {'form': form})
