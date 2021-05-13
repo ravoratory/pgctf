ab3e2f404ba55e902fd08f7316c0dabe8ff82d64
disVill <konokou2000@gmail.com>
2021-05-13 21:07:41 +0900

    Fix settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -132,9 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, '/static/'),
-)
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
