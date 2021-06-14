272256f0aa42454941ba8f64a936986b4e6ee979
disVill <konokou2000@gmail.com>
2021-06-15 01:52:04 +0900

    S3 upload settings

--- app/app/prod.py
+++ app/app/prod.py
@@ -18,3 +18,13 @@ STATICFILES_DIRS = (
     ('js', os.path.join(STATIC_ROOT, 'js')),
     ('html', os.path.join(STATIC_ROOT, 'html')),
 )
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
--- app/app/urls.py
+++ app/app/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path('signout/', auth_view.LogoutView.as_view(), name='signout'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
