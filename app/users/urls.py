5f81d2d382deaed5556829dc42a517c4fba3d283
Ravie403 <info@tkm.blue>
2022-07-14 13:11:40 +0900

    remove signup

--- app/users/urls.py
+++ app/users/urls.py
@@ -6,7 +6,6 @@ from . import views
 
 app_name = 'users'
 urlpatterns = [
-    path('signup/', views.SignUpView.as_view(), name='signup'),
     path('signin/', views.SignInView.as_view(), name='signin'),
     path('signout/', auth_view.LogoutView.as_view(), name='signout'),
 ]
