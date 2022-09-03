ead0dc31ac331cb4389938511b530e885dd46c09
Ravie403 <info@tkm.blue>
2022-09-03 19:14:35 +0900

    fix

--- app/announcements/views.py
+++ app/announcements/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.db.models import Sum
 
-from app.quizzes.models import Solved
+from quizzes.models import Solved
 from .models import Main, Announcement
 
 
@@ -14,4 +14,7 @@ class AnnouncementsView(LoginRequiredMixin, generic.ListView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
         context['main'] = Main.objects.first()
+        user = self.request.user
+        user.points = Solved.objects.filter(user=user, quiz__published=True).aggregate(points=Sum('quiz__point'))['points'] or 0
+        context['user'] = user
         return context
