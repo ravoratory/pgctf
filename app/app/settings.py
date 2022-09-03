15d48114f69a9aa8bc8a479e18a0f7647ff6bb3e
Ravie403 <info@tkm.blue>
2022-09-03 09:51:53 +0900

    add static root

--- app/app/settings.py
+++ app/app/settings.py
@@ -147,6 +147,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 STATICFILES_FINDERS = [
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
