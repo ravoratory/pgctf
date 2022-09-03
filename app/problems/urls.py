f84496de510838aa61126654d91dc2c47b9cc368
Ravie403 <info@tkm.blue>
2022-09-03 12:13:57 +0900

    add ex-c problem

--- app/problems/urls.py
+++ app/problems/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'problems'
 urlpatterns = [
     path('only_limited_host', views.only_limited_host, name='only_limited_host'),
+    path('check_admin', views.check_admin, name='check_admin')
 ]
 
--- app/problems/views.py
+++ app/problems/views.py
@@ -1,8 +1,13 @@
+from email.mime import base
+import re
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_GET
+from django.views.decorators.http import require_GET, require_http_methods
 from django.http import HttpRequest
 import os
+import base64
+import json
+
 
 @login_required
 @require_GET
@@ -12,3 +17,24 @@ def only_limited_host(request: HttpRequest):
         return render(request, 'problems/success.html', {'flag': os.getenv('FLAG_O', '')})
     else:
         return render(request, 'problems/error.html')
+
+@login_required
+@require_http_methods(['GET', 'POST'])
+def check_admin(request: HttpRequest):
+    admin_cookie = request.COOKIES.get("exc")
+    try:
+        decoded_cookie = json.loads(base64.b64decode(admin_cookie).decode('utf-8'))
+    except:
+        decoded_cookie = {'admin': False}
+    is_admin = decoded_cookie['admin']
+    if request.method == 'POST':
+        message = {'admin': is_admin, 'username': request.POST['username']}
+        response = render(request, 'problems/exc.html', {'message': str(message)})
+        cookie = json.dumps(message)
+        response.set_cookie('exc', base64.b64encode(cookie.encode()).decode())
+        return response
+    if is_admin:
+        message = os.getenv('FLAG_EXC')
+    else:
+        message = {'message': 'please login any account.'}
+    return render(request, 'problems/exc.html', {'message': str(message)})
