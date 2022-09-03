b9e25d3e24d3d6728d5d5f059e90a7e864fe96c9
disVill <konokou2000@gmail.com>
2022-09-03 22:52:24 +0900

    Webhook通知の作成

--- app/announcements/apps.py
+++ app/announcements/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AnnouncementsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'announcements'
+
+    def ready(self):
+        from . import signals  # noqa: F401
---
+++ app/announcements/signals.py
@@ -0,0 +1,79 @@
+import requests
+import json
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Announcement
+from quizzes.models import Quiz, Solved
+from users.models import User
+
+
+webhook_system_notify_url = settings.DISCORD_WEBHOOK_SYSTEM_NOTIFY_URL
+webhook_solved_notify_url = settings.DISCORD_WEBHOOK_SOLVED_NOTIFY_URL
+
+
+def discord_webhook_sender(payload, webhook_url):
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.post(webhook_url, data=json.dumps(payload), headers=headers)
+
+    return response
+
+
+@receiver(post_save, sender=Quiz)
+def quiz_create_receiver(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    payload = {
+        "content": ":question: New quiz created!",
+        "embeds": [{
+            "fields": [{
+                "name": f"{instance.quiz_number}: [{instance.category.category_name}]",
+                "value": instance.title,
+            }]
+        }],
+    }
+    discord_webhook_sender(payload, webhook_system_notify_url)
+
+
+@receiver(post_save, sender=Solved)
+def quiz_solved_receiver(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    payload = {
+        "content": f":partying_face: {instance.user.username} solved {instance.quiz.quiz_number}: {instance.quiz.title}"
+    }
+    discord_webhook_sender(payload, webhook_solved_notify_url)
+
+
+@receiver(post_save, sender=Announcement)
+def announcement_create_receiver(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    payload = {
+        "content": ":microphone: New announcement created!",
+        "embeds": [{
+            "fields": [{
+                "name": instance.title,
+                "value": instance.body,
+            }]
+        }],
+    }
+    discord_webhook_sender(payload, webhook_system_notify_url)
+
+
+@receiver(post_save, sender=User)
+def user_register_receiver(sender, instance, created, **kwargs):
+    if not created:
+        return
+
+    payload = {
+        "content": f":tada: @{instance.username} has been registered!"
+    }
+    discord_webhook_sender(payload, webhook_system_notify_url)
