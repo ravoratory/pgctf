baea1928aca928b2fd9a392e1a41c06085597e5d
Ravie403 <info@tkm.blue>
2022-09-03 09:45:05 +0900

    move flag to env

--- app/problems/views.py
+++ app/problems/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET
 from django.http import HttpRequest
-
+import os
 
 @login_required
 @require_GET
 def only_limited_host(request: HttpRequest):
     host: str = request.get_host()
     if host == 'example.com':  # ?????
-        return render(request, 'problems/success.html')
+        return render(request, 'problems/success.html', {'flag': os.getenv('FLAG_O', '')})
     else:
         return render(request, 'problems/error.html')
