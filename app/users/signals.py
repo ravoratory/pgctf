1a27eb20f0b6a7d52fac1a6ab60800cbfbc092e8
disVill <konokou2000@gmail.com>
2022-09-04 15:01:40 +0900

    signalの動作の修正

--- app/users/signals.py
+++ app/users/signals.py
@@ -6,12 +6,14 @@ from .models import User
 from quizzes.models import Quiz, Solved
 
 
-def recalculate_quiz_score():
+def recalculate_quiz_score(quiz_id=None):
     user_count = User.objects.filter(is_staff=False).count()
-    quizzes = Quiz.objects.filter(published=True).prefetch_related('solved_users')
-
     if user_count == 0:
         return
+
+    quizzes = Quiz.objects.filter(published=True).prefetch_related('solved_users')
+    if quiz_id is not None:
+        quizzes = quizzes.filter(id=quiz_id)
 
     for quiz in quizzes:
         solved_user_count = quiz.solved_users.filter(is_staff=False).count()
@@ -25,11 +27,6 @@ def user_signed_up_callback(**kwargs):
     recalculate_quiz_score()
 
 
-@receiver(post_save, sender=Solved)
-def solved_post_save_callback(**kwargs):
-    recalculate_quiz_score()
-
-
-@receiver(post_delete, sender=Solved)
-def solved_post_delete_callback(**kwargs):
-    recalculate_quiz_score()
+@receiver([post_save, post_delete], sender=Solved)
+def solved_post_delete_callback(sender, instance, **kwargs):
+    recalculate_quiz_score(quiz_id=instance.quiz.id)
